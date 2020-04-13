09d5458b50f7bba348f546486425c11998736132
JoelGunnarsson <joegun293@edu.linkoping.se>
2020-04-13 11:18:42 +0200

    Refactor edit med DocBlock

---
+++ app/views/posts/index.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "<br>Visa alla poster";
+echo "<br>view för PostsController::index";
--- public/index.php
+++ public/index.php
@@ -1,3 +1,63 @@
 <?php
+require "../app/bootstrap.php";
+//var_dump($_SERVER);
 
-echo "hello from index";
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+//    $r->addRoute('GET', '/', 'homeController.php');
+
+    //Visa alla posts
+    $r->addRoute('GET', '/posts', 'PostsController@index');
+    $r->addRoute('GET', '/posts/{id:\d+}[/]', 'PostsController@show');
+    $r->addRoute('GET', '/posts/{id:\d+}/edit[/]', 'PostsController@edit');
+    $r->addRoute('GET', '/posts/{id:\d+}/delete[/]','PostsController@delete');
+    $r->addRoute('POST','/posts/{id:\d+}[/]', 'PostsController@update');
+    $r->addRoute('GET', '/posts/new[/]', 'PostsController@add');
+    $r->addRoute('POST','/posts/new[/]', 'PostsController@store');
+
+//    // {id} must be a number (\d+)
+//    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+//    // The /{title} suffix is optional
+//    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        echo "404 hittades inte";
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        echo "Rutt hittad men fel protokoll";
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+//        echo "Hittade!";
+//    var_dump($handler);
+    $tmp = explode("@",$handler);
+//    var_dump($tmp);
+    $class = $tmp[0];
+    $method = $tmp[1];
+//        require CONTROLLERS . DIRECTORY_SEPARATOR . $class . ".php";
+    $controller = new $class;
+    $controller->$method($vars);
+
+
+
+    //        require "../app/" . $handler;
+        break;
+}
